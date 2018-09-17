c4132381b9c871f5407a07f21de95bc9b2e33d93
alexthayn <alexthayn>
2018-09-16 20:38:50 -0600

    first commit

--- HW03_AThayn/MainWindow.xaml.cs
+++ HW03_AThayn/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
 using System.IO;
+using System.Windows.Ink;
 
 namespace HW03_AThayn
 {
@@ -23,60 +24,136 @@ namespace HW03_AThayn
     /// </summary>
     public partial class MainWindow : Window
     {
-        byte[] imageArray;
+        public SolidColorBrush SelectedColor { get; set; } = Brushes.Black;
+        public enum SELECTEDTOOL { LARGEBRUSH=0, BRUSH=1, PENCIL=2, ERASER=3 }
+        public int SelectedTool = 2;
+        Dictionary<TouchDevice, Ellipse> _Followers = new Dictionary<TouchDevice, Ellipse>();
+        private StylusPointCollection stylusPoints;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public Brush SelectedColor { get; set; } = Brushes.White;
-
-        public byte[] ImageToBitmap(Image img)
-        {
-            //return (byte[])(new ImageSourceConverter()).ConvertTo(img, typeof(byte[]));
-            //return System.IO.File.ReadAllBytes((img.Source as BitmapImage).UriSource.OriginalString);
-            // Not developed yet.
-            throw new NotImplementedException();
-        }
-
         public void BtnColorSelected(object sender, RoutedEventArgs e)
         {
             Button btnSelected = (Button)sender;
-            SelectedColor = btnSelected.Background;
+            SelectedColor = (SolidColorBrush)btnSelected.Background;
             RctColorSelected.Fill = SelectedColor;
+            strokeAttr.Color = SelectedColor.Color;
         }
 
-        public void BtnPageSelected(object sender, RoutedEventArgs e)
+        public void BtnToolSelected(object sender, RoutedEventArgs e)
         {
-            Button button = (Button)sender;
-            Image image = new Image();
-
-            switch(button.Content)
+            Button btnSelected = (Button)sender;
+            switch(btnSelected.Name)
             {
-                case "Llama":
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Llama.jpg"));
+                case "Brush":
+                    SelectedTool = (int)SELECTEDTOOL.BRUSH;
+                    DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    strokeAttr.Width = 10;
+                    strokeAttr.Height = 10;
                     break;
-                case "Bear":
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Bear.jpg"));
+                case "LargeBrush":
+                    SelectedTool = (int)SELECTEDTOOL.LARGEBRUSH;
+                    DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    strokeAttr.Width = 20;
+                    strokeAttr.Height = 20;
                     break;
-                case "Bird":
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Bird.jpg"));
-                    break;
-                case "Turtle":
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Turtle.jpg"));
-                    break;
-                case "Fish":
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Fish.jpg"));
+                case "Eraser":
+                    SelectedTool = (int)SELECTEDTOOL.ERASER;
+                    DrawingCanvas.EditingMode = InkCanvasEditingMode.EraseByPoint;
+                    strokeAttr.Width = 10;
+                    strokeAttr.Height = 10;
                     break;
                 default:
-                    image.Source = new BitmapImage(new Uri("pack://application:,,,/ColoringPages/Pig.gif"));
-                    break;                   
-            }
+                    SelectedTool = (int)SELECTEDTOOL.PENCIL;
+                    DrawingCanvas.EditingMode = InkCanvasEditingMode.Ink;
+                    strokeAttr.Width = 3;
+                    strokeAttr.Height = 3;
+                    break;
+            }            
+        }
 
-            //imageArray = ImageToBitmap(image);
-            image.Stretch = Stretch.Uniform;
-            ColoringPage.Source = image.Source;
+        private void DrawingCanvas_OnTouchMove(object sender, TouchEventArgs e)
+        {
+            if (e.TouchDevice.Captured == DrawingCanvas)
+            {
+                Ellipse follower = _Followers[e.TouchDevice];
+                TranslateTransform transform = follower.RenderTransform as TranslateTransform;
+
+                TouchPoint point = e.GetTouchPoint(DrawingCanvas);
+
+                transform.X = point.Position.X;
+                transform.Y = point.Position.Y;
+            }
+        }
+
+        private void DrawingCanvas_OnTouchUp(object sender, TouchEventArgs e)
+        {
+            DrawingCanvas.ReleaseTouchCapture(e.TouchDevice);
+
+            DrawingCanvas.Children.Remove(_Followers[e.TouchDevice]);
+            _Followers.Remove(e.TouchDevice);
+        }
+
+        private void DrawingCanvas_OnTouchDown(object sender, TouchEventArgs e)
+        {
+            DrawingCanvas.CaptureTouch(e.TouchDevice);
+
+            Ellipse follower = new Ellipse();
+            follower.Width = follower.Height = 50;
+            follower.Fill = SelectedColor;
+            follower.Stroke = SelectedColor;
+
+            TouchPoint point = e.GetTouchPoint(DrawingCanvas);
+
+            follower.RenderTransform = new TranslateTransform(point.Position.X, point.Position.Y);
+
+            _Followers[e.TouchDevice] = follower;
+
+            DrawingCanvas.Children.Add(follower);
+        }
+
+        private void DrawingCanvas_StylusDown(object sender, StylusDownEventArgs e)
+        {
+            Stylus.Capture(this);
+
+            stylusPoints = new StylusPointCollection();
+            StylusPointCollection eventPoints =
+                e.GetStylusPoints(this, stylusPoints.Description);
+
+            stylusPoints.Add(eventPoints);
+        }
+
+        private void DrawingCanvas_StylusUp(object sender, StylusEventArgs e)
+        {
+            if (stylusPoints == null)
+            {
+                return;
+            }
+            
+            StylusPointCollection newStylusPoints =
+                e.GetStylusPoints(this, stylusPoints.Description);
+            stylusPoints.Add(newStylusPoints);
+            
+            Stroke stroke = new Stroke(stylusPoints);
+
+            stylusPoints = null;
+            
+            Stylus.Capture(null);
+        }
+
+        private void DrawingCanvas_StylusMove(object sender, StylusEventArgs e)
+        {
+            if (stylusPoints == null)
+            {
+                return;
+            }
+            
+            StylusPointCollection newStylusPoints =
+                e.GetStylusPoints(this, stylusPoints.Description);
+            stylusPoints.Add(newStylusPoints);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ColoringBookApp
+# PaintApp
